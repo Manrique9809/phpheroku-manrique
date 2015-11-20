b3cc7fbdd7d4671672f7246a4c631ef76a8904d1
Antony Manrique <antony476@gmail.com>
2015-11-20 12:17:10 -0500

    formularios en pryecto herokku

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- Latest compiled and minified CSS & JS -->
+	<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<title>dasdasdasd</title>
 	<link rel="stylesheet" href="">
 </head>
@@ -11,6 +15,20 @@
 	<form class="navbar-form pull-right">
 		<input type="text" style="width: 200px;">
 		<button type="submit" class="btn btn-default">Submit</button>
+	</form>
+
+	<form action="" method="POST" class="form-horizontal" role="form">
+			<div class="form-group">
+				<legend>Form title</legend>
+			</div>
+	
+			
+	
+			<div class="form-group">
+				<div class="col-sm-10 col-sm-offset-2">
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+			</div>
 	</form>
 </body>
 </html>
