9e3e44c56aa7d27cc62e897305d813e619587379
Antony Manrique <antony476@gmail.com>
2015-11-20 12:15:07 -0500

    formulario en proyecto heroku

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 </head>
 <body>
 	<h1>bienbenido a heroku</h1>
+	<form class="navbar-form pull-right">
+		<input type="text" style="width: 200px;">
+		<button type="submit" class="btn btn-default">Submit</button>
+	</form>
 </body>
 </html>
